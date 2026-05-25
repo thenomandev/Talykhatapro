8e7b8eb8dad68767e442cdcb07f91e9e2d70bab1
Abdullah-Al Noman <81487006+thenomandev@users.noreply.github.com>
2026-05-25 12:56:42 +0600

    Delete report.js

--- report.js
+++
@@ -1,5 +0,0 @@
-const reportViewContainer = document.getElementById("reportViewContainer");
-const closeReportBtn = document.getElementById("closeReportBtn");
-const reportTxnList = document.getElementById("reportTxnList");
-const reportTotalGave = document.getElementById("reportTotalGave");
-const reportTotalGot = document.getElementById("reportTotalGot");
